e1afb4bf8fca6c37c656e782522856457ac5b617
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-10-06 09:11:18 +0200

    Fixed user list

--- react-gym-app/src/components/UserList.tsx
+++ react-gym-app/src/components/UserList.tsx
@@ -2,18 +2,9 @@ import React from 'react';
 import { GymClass } from '../types/gymClassType';
 import { User } from '../types/userType';
 
-// interface User {
-//   username: string;
-//   isAdmin: boolean;
-//   //   bookedGymClasses: {
-//   //     gymClassName: string;
-//   //     date: string; // You can choose the appropriate date format here
-//   //   }[];
-// }
-
 interface UserListProps {
   users: User[];
-  gymClasses: GymClass[]; // Pass the gym classes as a prop
+  gymClasses: GymClass[];
 }
 
 const UserList: React.FC<UserListProps> = ({ users, gymClasses }) => {
@@ -39,36 +30,13 @@ const UserList: React.FC<UserListProps> = ({ users, gymClasses }) => {
                     </li>
                   );
                 }
-                return null; // Handle the case where a gym class with the ID is not found
+                return null;
               })}
             </ul>
           </li>
         ))}
       </ul>
     </div>
-
-    // <div>
-    //   <h2>User List</h2>
-    //   <ul>
-    //     {users.map((user, index) => (
-    //       <li key={index}>
-    //         Username: {user.username}, Role: {user.isAdmin ? 'Admin' : 'User'}
-    //         {/* {user.bookedGymClasses.length > 0 && (
-    //           <div>
-    //             <strong>Booked Gym Classes:</strong>
-    //             <ul>
-    //               {user.bookedGymClasses.map((gymClass, gymIndex) => (
-    //                 <li key={gymIndex}>
-    //                   Gym Class: {gymClass.gymClassName}, Date: {gymClass.date}
-    //                 </li>
-    //               ))}
-    //             </ul>
-    //           </div>
-    //         )} */}
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </div>
   );
 };
 
